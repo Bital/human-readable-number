6f170a2499688fc120f497c97a34c2e13986460b
Vitali Kuzmich <v.kuzmich@mail.ru>
2020-03-06 12:43:46 +0300

    feat: human readable number task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function toReadable (number) {
-  
+
+	if(number == 0) {
+		return 'zero';
+	}
+
+	const numbers = [
+	    ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'],
+		['ten', 'eleven', 'twelve',  'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'],
+		['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'],
+	]
+
+	
+	let strNumber = number.toString();
+	let order = strNumber.length - 1;
+
+	const converter = {
+        '2': (val) =>  [numbers[2][val] + ' hundred'].concat(converter['1'](strNumber.substr(-2, 1))),
+        '1': (val) =>  (val == 1) ? [numbers[1][strNumber.substr(-1, 1)]] : [numbers[0][val]].concat(converter['0'](strNumber.substr(-1, 1))),
+        '0': (val) =>  [numbers[2][val]]
+    }
+
+	return  converter[order](strNumber.substr(-(strNumber.length), 1)).filter((val) => val).join(' ');
 }
+
+
+	
+
